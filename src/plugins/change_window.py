53852225198f0664f58f58cf4705b2453e3e226b
Xavier Mitault <xavier.mitault@epitech.eu>
2022-06-09 20:51:13 +0000

    Fix switch to different window

--- src/plugins/change_window.py
+++ src/plugins/change_window.py
@@ -1,45 +1,24 @@
 import os
 import pyautogui
-# import xdoctl
-# import subprocess
-# import shlex
+from xdo import Xdo
 
 ok_text = ["go to", "va"]
 
-# def exec_return_ouput_list(li: list) -> list:
-#     try:
-#         ret = subprocess.check_output(li, timeout=2)
-#     except subprocess.TimeoutExpired:
-#         return []
-#     return (ret.decode('utf-8')).splitlines()
-#
-# def exec_return_ouput(command: str) -> list:
-#     return exec_return_ouput_list(shlex.split(command))
-#
-# def change_window() -> bool:
-#     app = pyautogui.prompt("Which application do you want to go ?")
-#     if app == None:
-#         return False
-#     print(f"app : {app}")
-#     win_ids = exec_return_ouput(f"xdotool search --sync --onlyvisible --maxdepth 2 --name {app}")
-#     print(f"win_ids : {win_ids}")
-#     if len(win_ids) == 0 or win_ids[0] == "":
-#         return False
-#     exec_return_ouput(f"xdotool windowactivate {win_ids[0]}")
-#     print("xddd")
-#     return True
-
-# def change_window() -> bool:
-#     app = pyautogui.prompt("Which application do you want to go ?")
-#     if app == None:
-#         return False
-#     res = xdoctl.window.search(app ,sync=True, only_visible=True, match_all=True)
-#     if res == None or len(res) == 0:
-#         return False
-#     xdoctl.window.window_focus(res[0], sync=True)
-#     return True
-
 def change_window() -> bool:
+    app: str = pyautogui.prompt("Which application do you want to go ?")
+    if app == None:
+        return False
+    x = Xdo()
+    nb_deskt = x.get_number_of_desktops()
+    cur_deskt = x.get_current_desktop()
+    for i in range(1, nb_deskt + 1):
+        x.set_current_desktop(i)
+        res = x.search_windows(bytes(app, 'utf-8'), only_visible=True, max_depth=2, require=True)
+        if res != None and len(res) != 0:
+            print(res)
+            x.activate_window(res[-1])
+            return True
+    x.set_current_desktop(cur_deskt)
     return False
 
 def plug_change_window(text: str):
